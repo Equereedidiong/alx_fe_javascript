2970aef8be21dcd60e6c47a4991bc1563b5095cb
A'DI <EquereAedidiong@gmail.com>
2024-09-07 16:43:25 +0100

    Update for dom

--- dom-manipulation/notekeeper.js
+++ dom-manipulation/notekeeper.js
@@ -5,11 +5,11 @@ const BtnSelector = document.querySelector('#Notebutton');
 const note = document.querySelector("#note");
 
 const allNotes = [];
-''
-const noteL = document.getElementById('noteLogger');
-par = document.createElement('p');
-noteL.appendChild(par)
 
+const noteL = document.getElementById('noteLogger');
+// par = document.createElement('p');
+// noteL.appendChild(par)
+const noteSection = document.querySelector('.note-section');
 
 
 
@@ -19,14 +19,41 @@ BtnSelector.addEventListener ('click', (Submit) => {
 
     // console.log(note.value);
     allNotes.push(note.value);
-    console.log(allNotes);
-
+    // console.log(allNotes);
+     
     // const noteL = document.getElementById('noteLogger');
-    // par = document.createElement("p")
-    // noteL.appendChild(par)
+    let paragraph = document.createElement('p');
+    paragraph.textContent = note.value;
+    noteSection.appendChild(paragraph);
 
+    // If the oaragraph must have a style you early have defined just
+    // do Paragraph.classList.add("note");
+    
+    //To store the items locally
 
-
-
+    localStorage.setItem('notes', JSON.stringify(allNotes));
+    // This would make the values of notes in string format
+    note.value = "";
 });
 
+//We need a function that fetches our notes and renders them in Javascript array objects;
+const renderNote = () => {
+    const notes1 = localStorage.getItem('notes');
+    //To get our data in a JSON form that is useable by Javascript we then, parse the value.
+    const gottenData = JSON.parse(notes1);
+    // allNotes.push(...gottenData)
+    // console.log(gottenData);
+    allNotes.push(...gottenData); // (...gottenData is a spread Operator, that helps )
+
+    gottenData.map((note) => {
+    
+    let paragraph = document.createElement('p');
+    paragraph.textContent = note;
+    noteSection.appendChild(paragraph);
+    // When we ran the code at this point the code cleared after a new session was created
+    // because at the beginning we defined notes = []; so now we have to push the existing 
+    //notes to the one stored in local storage.
+    });
+};
+
+renderNote();
