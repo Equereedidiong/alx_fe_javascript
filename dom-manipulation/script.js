f67485feece5187f4089c35ac379527e877bfff9
A'DI <EquereAedidiong@gmail.com>
2024-09-08 07:23:58 +0100

    Export json

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -113,3 +113,16 @@ function exportQuotesToJSON() {
 
 // Event listener for the export button
 document.getElementById('export-btn').addEventListener('click', exportQuotesToJS
+
+// JSON Import
+
+function importFromJsonFile(event) {
+  const fileReader = new FileReader();
+  fileReader.onload = function(event) {
+    const importedQuotes = JSON.parse(event.target.result);
+    quotes.push(...importedQuotes);
+    saveQuotes();
+    alert('Quotes imported successfully!');
+  };
+  fileReader.readAsText(event.target.files[0]);
+}
